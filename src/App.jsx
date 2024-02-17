bcc75df8825c09e5a3f7784964a2ecdc95035ba3
Alex <alemumu207@gmail.com>
2024-02-17 11:53:04 +0100

    Creando fondo de la aplicacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import Card from "./components/Card"
+import imagen from './assets/images/background-pattern-desktop.svg';
 
 function App() {
 
   return (
-    <>
+    <div className="main-container">
+      <img className="img-background" src={imagen} alt="Imagen fondo superior" />
       <Card/>
-      From APP
-    </>
+    </div>
   )
 }
 
